b28c7935e97badd0799e52fff397f28cc10325a3
SHUN <storyfromshiori@gmail.com>
2025-08-21 22:19:57 +0900

    feat: Enable PWA functionality

---
+++ sw.js
@@ -0,0 +1,38 @@
+// 役割: 必要ファイルをキャッシュしてオフライン配信
+const CACHE_NAME = 'character-create-v1';
+const ASSETS = [
+    './',
+    './characterCreate.html',
+    './manifest.webmanifest',
+    './assets/Wizard_OldMan_Front_Pixel.png',
+    './icons/CharacterCreate_Logo_192x192.png',
+    './icons/CharacterCreate_Logo_512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    const { request } = event;
+    event.respondWith(
+        caches.match(request).then((cached) =>
+            cached || fetch(request).then((res) => {
+                // GET のみキャッシュ（POST 等は除外）
+                if (request.method === 'GET' && res.ok) {
+                    const copy = res.clone();
+                    caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+                }
+                return res;
+            }).catch(() => caches.match('./characterCreate.html')) // ネット不通時のフォールバック
+        )
+    );
+});
