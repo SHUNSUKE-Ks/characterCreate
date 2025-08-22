80157161ae69cd26c12bb8efd19dc1c614c1a71d
SHUN <storyfromshiori@gmail.com>
2025-08-22 09:48:45 +0900

    'feat:Add_secure_Firebase_config_and_build_script'

---
+++ generate-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script generates the firebase-config.js file from Vercel's environment variables.
+
+echo "const firebaseConfig = {
+  apiKey: '$VITE_FIREBASE_API_KEY',
+  authDomain: '$VITE_FIREBASE_AUTH_DOMAIN',
+  projectId: '$VITE_FIREBASE_PROJECT_ID',
+  storageBucket: '$VITE_FIREBASE_STORAGE_BUCKET',
+  messagingSenderId: '$VITE_FIREBASE_MESSAGING_SENDER_ID',
+  appId: '$VITE_FIREBASE_APP_ID'
+};" > firebase-config.js
